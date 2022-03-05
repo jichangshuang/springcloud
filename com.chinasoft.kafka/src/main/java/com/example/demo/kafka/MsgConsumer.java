16bf08c698837392ad0c42c1676d3ed34f5f3c1a
ji.changshuang <863334068@qq.com>
2022-03-05 15:26:33 +0800

    kafka

---
+++ com.chinasoft.kafka/src/main/java/com/example/demo/kafka/MsgConsumer.java
@@ -0,0 +1,39 @@
+package com.example.demo.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+public class MsgConsumer {
+    public static void main(String[] args) {
+        Properties p = new Properties();
+        p.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        // 消费分组名
+        p.put(ConsumerConfig.GROUP_ID_CONFIG, "test");
+        // 是否自动提交offset
+        p.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
+        // 自动提交offset的间隔时间
+        p.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+        p.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        p.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(p);
+        consumer.subscribe(Arrays.asList("my-topic"));
+        while (true) {
+            // poll 是拉取消息的长轮询
+            ConsumerRecords<String, String> records = consumer.poll(1000);
+            for (ConsumerRecord record : records) {
+                System.out.println("我收到消息了"+record.offset() + "  " + record.key() + " " + record.value());
+            }
+            if(records.count() > 0) {
+                
+            }
+        }
+
+    }
+}
---
+++ com.chinasoft.kafka/src/main/java/com/example/demo/kafka/MsgProducer.java
@@ -0,0 +1,34 @@
+package com.example.demo.kafka;
+
+import org.apache.kafka.clients.producer.*;
+
+import java.util.Properties;
+
+public class MsgProducer {
+
+    public static void main(String[] args) {
+        Properties p = new Properties();
+        p.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        p.put(ProducerConfig.ACKS_CONFIG, "1");
+        p.put(ProducerConfig.RETRIES_CONFIG, 3);
+        p.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+        p.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
+        p.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432); //32M
+        p.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        p.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<String, String>(p);
+
+        for (int i = 0; i < 5; i++) {
+            producer.send(new ProducerRecord<String, String>("my-topic", Integer.toString(i)), new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    System.out.println("同步方式发送消息结果: " + recordMetadata.topic() + "| partition-" + recordMetadata.partition() + "| offset-" + recordMetadata.offset());
+
+                }
+            });
+        }
+
+
+    }
+}
