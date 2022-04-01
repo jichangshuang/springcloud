2086c64e8d0ef91f6fcf4a1c4c27bea5b4e98c6a
ji.changshuang <863334068@qq.com>
2022-04-01 21:35:58 +0800

    seata

--- com.chinasoft.sentinel/src/main/java/com/chinasoft/sentinel/SentinelApplication.java
+++ com.chinasoft.sentinel/src/main/java/com/chinasoft/sentinel/SentinelApplication.java
@@ -1,6 +1,5 @@
 package com.chinasoft.sentinel;
 
-import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import com.alibaba.nacos.spring.context.annotation.discovery.EnableNacosDiscovery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
