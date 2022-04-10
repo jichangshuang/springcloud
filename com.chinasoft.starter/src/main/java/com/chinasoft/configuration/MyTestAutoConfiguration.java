825bf66c0b481db0b2b1d8575c00fc4e8a23b56a
ji.changshuang <863334068@qq.com>
2022-04-10 17:23:41 +0800

    自定义starter

---
+++ com.chinasoft.starter/src/main/java/com/chinasoft/configuration/MyTestAutoConfiguration.java
@@ -0,0 +1,11 @@
+package com.chinasoft.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyTestAutoConfiguration {
+
+//    @Bean
+
+}
